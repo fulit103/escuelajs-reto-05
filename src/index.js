88594191c7d2b18c751ef631ee6a51db4780a51e
Julian Toro <jtoro@tecmovin.com>
2019-09-28 11:15:29 -0500

    reto 1, reto 2, reto 3, reto 4

--- src/index.js
+++ src/index.js
@@ -1,30 +1,68 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://rickandmortyapi.com/api/character/?page=23';
 
-const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+const renderCharacters = function (characters){
+  let output = characters.map(character => {
+    return `
+  <article class="Card">
+    <img src="${character.image}" />
+    <h2>${character.name}<span>${character.species}</span></h2>
+  </article>
+`
+  }).join('');
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
 }
 
-const loadData = () => {
-  getData(API);
+class Client{
+  constructor(api) {
+    this.api = api;
+    localStorage.setItem("next_fetch", "")
+  }
+
+  getData() {
+    return new Promise((resolve,reject) => {
+      const api = localStorage.getItem("next_fetch")==="" ? this.api : localStorage.getItem("next_fetch")
+      fetch(api)
+        .then(response => response.json())
+        .then(response => {
+          if( response.info.next!="" ){
+            localStorage.setItem("next_fetch", response.info.next )          
+            resolve({
+              characters: response.results,
+              last: false
+            })
+          }
+          else{
+            resolve({
+              characters: response.results,
+              last: true
+            })
+          }          
+        })
+        .catch(error => reject(error))
+    })
+  }
+}
+
+const client = new Client(API);
+
+const loadData = async function() {
+  try{    
+    const {characters, last} = await client.getData()
+    renderCharacters(characters)
+    if(last==true){
+      let newItem = document.createElement('p');      
+      newItem.innerHTML = "Ya no hay personajes";
+      $app.appendChild(newItem);
+      intersectionObserver.unobserve($observe)
+    }
+  }catch(err){
+    console.log(err)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
